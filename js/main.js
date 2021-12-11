605488d2b96f872b2592336d24e78bc88a4ee3ec
Dimitris Kidis <cyraxmite@gmail.com>
2021-12-11 23:13:06 +0200

    Second Graph Logic Finished

--- js/main.js
+++ js/main.js
@@ -1328,11 +1328,64 @@ const config = {
       legend: {
         display: false
               },
-           }
+           },
   }
 };
 
 const myChart = new Chart(
   document.getElementById('myChart'),
   config
+);
+
+
+const labels2 = [JSON.parse(localStorage.getItem('fastestCPM')), JSON.parse(localStorage.getItem('averageAccuracy'))];
+
+
+const data2 = {
+  labels: ['', ''],
+  datasets: [{
+    label: ' ',
+    data: [JSON.parse(localStorage.getItem('fastestCPM')), JSON.parse(localStorage.getItem('averageAccuracy'))],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(201, 203, 207, 0.2)'
+    ],
+    borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(153, 102, 255)',
+      'rgb(201, 203, 207)'
+    ],
+    borderWidth: 1
+  }]
+};
+
+const config2 = {
+  type: 'bar',
+  data: data2,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    },
+    plugins:{   
+      legend: {
+        display: false
+              }
+            }
+  },
+};
+
+const myChart2 = new Chart(
+  document.getElementById('myChart2'),
+  config2
 );
